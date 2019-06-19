924a7aec33ef877a894cf48427b400b8b46097cd
cashcoder88 <cashglobe@gmail.com>
2019-06-19 15:42:30 -0600

    passed another test, shows the controls and display

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ The expected/assumed behavior of the application is listed below.
 ### Gate
 
 - defaults to `unlocked` and `open` [x]
-- cannot be closed or opened if it is locked []
+- cannot be closed or opened if it is locked [x]
 
 ### Dashboard
 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,15 +2,27 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Dashboard from './Dashboard'
-import Display from '../display/Display.js'
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
+
 
 describe('Dashboard', () => {
     
 
-    it('runs tests', () => {
+    it('runs tests sanity check', () => {
         expect(true).toBe(true);
       });
 
 
+      // This was the one we discussed yesterday in review, solved with Test Id!
+    it('shows the controls and display', () => {
+      const {getByTestId} = render(<Dashboard/>)
+
+      const controls = getByTestId(/controls/i)
+      const display = getByTestId(/display/i)
+      expect(display).toBeTruthy();
+      expect(controls).toBeTruthy();
+    });
 });
 
+// - shows the controls and display [x]
