4d95bfbf99e8fccc4a5e327db7130ef0ba6a7f10
cashcoder88 <cashglobe@gmail.com>
2019-06-19 13:50:20 -0600

    Added dependencies, started first tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,23 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+describe('Dashboard', () => {
+    
+
+    it('runs tests', () => {
+        expect(true).toBe(true);
+      });
+
+    // it('defaults to unlocked and open', () => {
+        
+    // });
+
+});
+
+/*
+- defaults to `unlocked` and `open` []
+- cannot be closed or opened if it is locked []
+
+*/
