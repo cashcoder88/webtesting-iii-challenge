fa42199fcf308ab7c57af0a6c73a14a73a15b754
cashcoder88 <cashglobe@gmail.com>
2019-06-19 19:52:11 -0600

    Added jest-dom, finished last tests, MVP completed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Controls from './Controls.js'
 import { fireEvent } from '@testing-library/react/dist';
+import 'jest-dom/extend-expect'
+import '@testing-library/react/cleanup-after-each';
 
 describe('Controls', () => {
     it('cannot be closed or opened if it is locked', () => {
@@ -23,12 +25,67 @@ describe('Controls', () => {
 
     });
 
+    it("buttons' text changes to reflect the state the door will be in if clicked", () => {
+        const {getByText} = render(<Controls closed={false}/>)
+
+        const button = getByText(/Close gate/i)
+
+
+        expect(button).toBeTruthy();
+
+    });
+
+    it("buttons' text changes to reflect the state the door will be in if clicked, 2", () => {
+        const {getByText} = render(<Controls closed={true}/>)
+
+        const button = getByText(/Open gate/i)
+
+
+        expect(button).toBeTruthy();
+
+    });
+
+    it("buttons' text changes to reflect the state the door will be in if clicked, 3", () => {
+        const {getByText} = render(<Controls locked={false}/>)
+
+        const button = getByText(/Lock gate/i)
+
+
+        expect(button).toBeTruthy();
+
+    });
+    it("buttons' text changes to reflect the state the door will be in if clicked, 4", () => {
+        const {getByText} = render(<Controls locked={true}/>)
+
+        const button = getByText(/Unlock gate/i)
+
+
+        expect(button).toBeTruthy();
+
+    });
+
+    it('the closed toggle button is disabled if the gate is locked', () => {
+        const {getByTestId} = render(<Controls locked={true}/>)
+
+        const button = getByTestId(/closedd/i)
+
+        expect(button).toHaveAttribute('disabled');
+    });
+
+    it('the locked toggle button is disabled if the gate is open', () => {
+        const {getByTestId} = render(<Controls closed={false}/>)
+
+        const button = getByTestId(/lockedd/i)
+
+        expect(button).toHaveAttribute('disabled');
+    });
+
 });
 
 
 
 // cannot be closed or opened if it is locked [x]
 // - provide buttons to toggle the `closed` and `locked` states.[x]
-// - buttons' text changes to reflect the state the door will be in if clicked []
-// - the closed toggle button is disabled if the gate is locked []
-// - the locked toggle button is disabled if the gate is open []
+// - buttons' text changes to reflect the state the door will be in if clicked [x]
+// - the closed toggle button is disabled if the gate is locked [x]
+// - the locked toggle button is disabled if the gate is open [x]
